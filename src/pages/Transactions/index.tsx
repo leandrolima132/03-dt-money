517760c4632057a4723e5d2d0720a2d5cb7f364d
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2024-08-16 15:13:22 -0300

    feat: create transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,29 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return(
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+
+        <TransactionsTable>
+          <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }

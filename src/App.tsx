f17b6d1de7825dbda68abe7f7559c190b9e68a17
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2024-08-16 12:59:38 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from './styles/themes/default'
+import { Transactions } from "./pages/Transactions"
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Transactions/>
+    </ThemeProvider>
   )
 }
 
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+
+export function Transactions() {
+  return(
+    <div>
+      <Header />
+      <h1>Transactions</h1>
+      </div>
+  )
+}
